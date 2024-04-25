9f556e04f8f04cc3c92d1a5a7cc05f88174af256
Gabriel <gabrielmornard@gmail.com>
2024-04-25 10:48:26 -0400

    REFACTOR Changement dans les insertions

---
+++ ProjetFinSessionBD/Sql_Scripts/V1_2__CreationVuePiloteParEcurie.sql
@@ -0,0 +1,9 @@
+﻿USE Formule1
+GO
+
+CREATE VIEW Participations.vw_EcurieEtPilote 
+AS
+	SELECT E.Nom AS [NomEcurie], P.Nom AS [NomPilote], P.Points, P.NBVictoire, E.EcurieID, P.PiloteID
+	FROM Participations.Ecurie E
+	INNER JOIN Participations.Pilote P ON P.EcurieID = E.EcurieID
+GO
