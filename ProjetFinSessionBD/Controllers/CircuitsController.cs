ce3036f84e47fc588fbca7ef9c997777e15ea1f7
Gabriel <gabrielmornard@gmail.com>
2024-05-08 08:35:38 -0400

    FCT Ajout du controller et view Circuits

---
+++ ProjetFinSessionBD/Controllers/CircuitsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjetFinSessionBD.Data;
+using ProjetFinSessionBD.Models;
+
+namespace ProjetFinSessionBD.Controllers
+{
+    public class CircuitsController : Controller
+    {
+        private readonly Formule1Context _context;
+
+        public CircuitsController(Formule1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Circuits
+        public async Task<IActionResult> Index()
+        {
+              return _context.Circuits != null ? 
+                          View(await _context.Circuits.ToListAsync()) :
+                          Problem("Entity set 'Formule1Context.Circuits'  is null.");
+        }
+
+        // GET: Circuits/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Circuits == null)
+            {
+                return NotFound();
+            }
+
+            var circuit = await _context.Circuits
+                .FirstOrDefaultAsync(m => m.CircuitId == id);
+            if (circuit == null)
+            {
+                return NotFound();
+            }
+
+            return View(circuit);
+        }
+
+        // GET: Circuits/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Circuits/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CircuitId,Nom,Pays,Ville")] Circuit circuit)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(circuit);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(circuit);
+        }
+
+        // GET: Circuits/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Circuits == null)
+            {
+                return NotFound();
+            }
+
+            var circuit = await _context.Circuits.FindAsync(id);
+            if (circuit == null)
+            {
+                return NotFound();
+            }
+            return View(circuit);
+        }
+
+        // POST: Circuits/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CircuitId,Nom,Pays,Ville")] Circuit circuit)
+        {
+            if (id != circuit.CircuitId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(circuit);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CircuitExists(circuit.CircuitId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(circuit);
+        }
+
+        // GET: Circuits/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Circuits == null)
+            {
+                return NotFound();
+            }
+
+            var circuit = await _context.Circuits
+                .FirstOrDefaultAsync(m => m.CircuitId == id);
+            if (circuit == null)
+            {
+                return NotFound();
+            }
+
+            return View(circuit);
+        }
+
+        // POST: Circuits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Circuits == null)
+            {
+                return Problem("Entity set 'Formule1Context.Circuits'  is null.");
+            }
+            var circuit = await _context.Circuits.FindAsync(id);
+            if (circuit != null)
+            {
+                _context.Circuits.Remove(circuit);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CircuitExists(int id)
+        {
+          return (_context.Circuits?.Any(e => e.CircuitId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ ProjetFinSessionBD/Sql_Scripts/V1_4__ProcedureStockeMasterDetail.sql
@@ -0,0 +1,14 @@
+﻿USE Formule1
+GO 
+CREATE PROCEDURE Evenements.usp_CircuitParEvenement
+(
+	@CircuitID int
+)
+AS
+BEGIN
+	SELECT C.Nom, C.Pays, C.Ville, E.Nom, E.Date
+	FROM Evenements.Circuit C
+	INNER JOIN Evenements.Evenement E ON E.CircuitId = C.CircuitId
+	WHERE C.CircuitId = @CircuitID
+END
+GO
